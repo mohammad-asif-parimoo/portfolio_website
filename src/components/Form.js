c99faed356ac7179317edb576fa6f2d304cb4603
Mohammad Asif Parimoo <mohammadasifparimoo@gmail.com>
2023-05-06 19:47:03 +0530

    Fixed some bugs and optimized the code

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,22 @@ import '../styles/FormStyles.css';
 
 const Form = () => {
     const [showAlert, setShowAlert] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setShowAlert(true);
-        window.scroll(0, 0);
-        window.addEventListener('resize', () => {
+        document.activeElement.blur();
+        setTimeout(() => {
             window.scrollTo(0, 0);
-        });
+        }, 200);
+        setShowAlert(true);
+        setName('');
+        setEmail('');
+        setSubject('');
+        setMessage('');
     };
 
     return (
@@ -23,13 +31,13 @@ const Form = () => {
             )}
             <form onSubmit={handleSubmit}>
                 <label>Name</label>
-                <input type="text" onFocus={(event) => event.target.select()} />
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} onFocus={(event) => event.target.select()} />
                 <label>Email</label>
-                <input type="email" onFocus={(event) => event.target.select()} />
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} onFocus={(event) => event.target.select()} />
                 <label>Subject</label>
-                <input type="text" onFocus={(event) => event.target.select()} />
+                <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} onFocus={(event) => event.target.select()} />
                 <label>Message</label>
-                <textarea rows="6" placeholder="Type your message here..." onFocus={(event) => event.target.select()} />
+                <textarea rows="6" placeholder="Type your message here..." value={message} onChange={(e) => setMessage(e.target.value)} onFocus={(event) => event.target.select()} />
                 <button className="my-btn" type="submit">
                     SUBMIT
                 </button>
