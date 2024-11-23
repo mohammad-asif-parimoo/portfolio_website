bc942a5f3ce17b6a78b37b8e017bffd943d072aa
Mohammad Asif Parimoo <mohammadasifparimoo@gmail.com>
2024-11-23 19:34:31 +0530

    Update About.js

--- client/src/routes/About.js
+++ client/src/routes/About.js
@@ -8,7 +8,7 @@ const About = () => {
     return (
         <div>
             <Navbar />
-            <HeroImg2 heading="ABOUT" paragraph="I'm a Full Stack Developer" />
+            <HeroImg2 heading="ABOUT" paragraph="I'm a DevOps Engineer" />
             <AboutContent />
             <Footer />
         </div>
