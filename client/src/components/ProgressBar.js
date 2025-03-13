d831f5b1e9738d9fe846ee8ad5ecbcb650d58e36
Mohammad Asif Parimoo <mohammadasifparimoo@gmail.com>
2025-03-13 18:02:13 +0530

    Update ProgressBar.js

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import "../styles/ProgressBarStyles.css"; // Import the CSS file
+import "../styles/ProgressBarStyles.css";
 
 const ProgressBar = () => {
     const [isVisible, setIsVisible] = useState(false);
@@ -13,7 +13,6 @@ const ProgressBar = () => {
                 }
             });
         });
-
         observer.observe(progressBarRef.current);
 
         return () => observer.disconnect();
@@ -23,44 +22,68 @@ const ProgressBar = () => {
         <div className="container">
             <h1 className="text-center">Skills</h1>
             <div className="row" ref={progressBarRef}>
-                {isVisible && (
-                    <div className="col-md-12">
-                        <Skill name="HTML" percentage={80} className="html" />
-                        <Skill name="CSS" percentage={70} className="css" />
-                        <Skill name="JavaScript" percentage={75} className="javascript" />
-                        <Skill name="Tailwind CSS" percentage={70} className="tailwind-css" />
-                        <Skill name="Bootstrap" percentage={75} className="bootstrap" />
-                        <Skill name="React JS" percentage={70} className="react-js" />
-                        <Skill name="Express JS" percentage={60} className="express-js" />
-                        <Skill name="Node JS" percentage={70} className="node-js" />
-                        <Skill name="MongoDB" percentage={75} className="mongo-db" />
-                        <Skill name="MySQL" percentage={70} className="mysql" />
-                        <Skill name="Docker" percentage={85} className="docker" />
-                        <Skill name="Jenkins" percentage={80} className="jenkins" />
-                        <Skill name="OWASP" percentage={70} className="owasp" />
-                        <Skill name="SonarQube" percentage={75} className="sonarqube" />
-                        <Skill name="Trivy" percentage={70} className="trivy" />
-                        <Skill name="Kubernetes" percentage={85} className="kubernetes" />
-                        <Skill name="ArgoCD" percentage={75} className="argocd" />
-                        <Skill name="Prometheus" percentage={80} className="prometheus" />
-                        <Skill name="Grafana" percentage={75} className="grafana" />
-                        <Skill name="AWS/EKS" percentage={80} className="aws_eks" />
-                    </div>
-                )}
+                {
+                    isVisible && (
+                        <div className="col-md-12">
+                            <h3 className="progress-title">HTML</h3>
+                            <div className="progress">
+                                <div className="progress-bar html" style={{ width: "80%" }}>
+                                    <div className="progress-value">80%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">CSS</h3>
+                            <div className="progress">
+                                <div className="progress-bar css" style={{ width: "70%" }}>
+                                    <div className="progress-value">70%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">JavaScript</h3>
+                            <div className="progress">
+                                <div className="progress-bar javaScript" style={{ width: "75%" }}>
+                                    <div className="progress-value">75%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">Tailwind CSS</h3>
+                            <div className="progress">
+                                <div className="progress-bar tailwind-css" style={{ width: "70%" }}>
+                                    <div className="progress-value">70%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">Bootstrap</h3>
+                            <div className="progress">
+                                <div className="progress-bar bootstrap" style={{ width: "75%" }}>
+                                    <div className="progress-value">75%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">React JS</h3>
+                            <div className="progress">
+                                <div className="progress-bar react-js" style={{ width: "70%" }}>
+                                    <div className="progress-value">70%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">Express JS</h3>
+                            <div className="progress">
+                                <div className="progress-bar express-js" style={{ width: "60%" }}>
+                                    <div className="progress-value">60%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">Node JS</h3>
+                            <div className="progress">
+                                <div className="progress-bar node-js" style={{ width: "70%" }}>
+                                    <div className="progress-value">70%</div>
+                                </div>
+                            </div>
+                            <h3 className="progress-title">MongoDB</h3>
+                            <div className="progress">
+                                <div className="progress-bar mongo-db" style={{ width: "75%" }}>
+                                    <div className="progress-value">75%</div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
             </div>
         </div>
-    );
-};
-
-const Skill = ({ name, percentage, className }) => (
-    <>
-        <h3 className="progress-title">{name}</h3>
-        <div className="progress">
-            <div className={`progress-bar ${className}`} style={{ width: `${percentage}%` }}>
-                <div className="progress-value">{percentage}%</div>
-            </div>
-        </div>
-    </>
-);
+    )
+}
 
 export default ProgressBar;
